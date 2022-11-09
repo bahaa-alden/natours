6648570a26a3c4bd95a0db9935f73e4493e5fb9d
bahaa-alden <samerabood195@gmail.com>
2022-11-09 20:19:47 +0200

    sss

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 ﻿const express = require('express');
+const fs = require('fs');
+const tours = require('./dev-data/data/tours-simple.json');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/api/v1/tours', (req, res) => {
+  res.json({
+    status: 200,
+    result: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
