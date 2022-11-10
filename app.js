2d2d844cd28c92ec63cad20cf5ab78c98a52b9d2
bahaa-alden <samerabood195@gmail.com>
2022-11-10 12:36:54 +0200

    ss

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const fs = require('fs');
 const tours = require('./dev-data/data/tours-simple.json');
 const app = express();
+app.use(express.json());
 const port = 3000;
 
 app.get('/api/v1/tours', (req, res) => {
@@ -12,5 +13,23 @@ app.get('/api/v1/tours', (req, res) => {
       tours,
     },
   });
+});
+app.post('/api/v1/tours', function (req, res) {
+  const id = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: id }, req.body);
+  tours.push(newTour);
+  fs.writeFile(
+    './dev-data/data/tours-simple.json',
+    JSON.stringify(tours),
+    (err) => {
+      console.log('done');
+      res.status(201).send({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
