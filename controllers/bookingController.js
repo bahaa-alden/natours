56b16b0ed0d7161cf2addaa0cc66e0d7676b3ab8
bahaa-alden-abdo <samerabood195@gmail.com>
2023-03-26 14:25:34 +0300

    improving stripe 4

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -63,7 +63,7 @@ export const getCheckoutSession = catchAsync(async (req, res, next) => {
 // });
 const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
-  const user = await User.findOne({ email: session.customer_email });
+  const user = await User.findOne({ email: session.customer_email }).id;
   const price = session.amount_subtotal / 100;
   console.log(tour, user, price);
   await Booking.create({ tour, user, price });
